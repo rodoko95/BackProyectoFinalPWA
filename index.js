6b766d7baf66ebd81eaef9f30ebb7d8d6aee81ca
Rodo <rodrigo.arce052@gmail.com>
2022-12-21 15:30:18 -0300

    Back Proyecto Final PWA

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ const rozenController = require('./controllers/rozenController');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('common'))
-const corsOptions ={
-    origin:'backproyectofinalpwa-production.up.railway.app', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: true })); 
 // app.use(bodyParser.json());
 
